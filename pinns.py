f93e94f88d9d190650231d42bd3a68630e1c4f89
canghiskhan <66562619+canghiskhan@users.noreply.github.com>
2023-11-04 18:57:14 -0500

    Add files via upload
    What we need to code first

---
+++ pinns.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+
+#Make true solution
+
+#Make the PINNs solution
+#Use https://towardsdatascience.com/physics-informed-neural-networks-pinns-an-intuitive-guide-fff138069563 and convert to pytorch
+
+
+#Solve with RK4 solver
+
+#compare accracy
