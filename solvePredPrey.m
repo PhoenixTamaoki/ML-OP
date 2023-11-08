81456e5692b15cfa07915463ba4065f4b53374dc
Calvin <calvinang@icloud.com>
2023-11-08 15:45:52 -0500

    changed parameters for stabile coexistence

--- solvePredPrey.m
+++ solvePredPrey.m
@@ -1,7 +1,7 @@
-alpha = 2; beta = 4/3; delta = 10; gamma = 1; tf = 10; 
+alpha = 2; beta = 4/3; delta = 4; gamma = 3; tf = 10; 
 tspan = [0 tf];
 f = @(t,y) [alpha*y(1)-beta*y(1)*y(2); delta*y(1)*y(2)-gamma*y(2)];
-[t,u] = ode45(f,[0 1],[10,10]);
+[t,u] = ode45(f,[0 tf],[2 1]);
 plot(t,u(:,1),'b-',t,u(:,2),'r-');
 
 %Take random points and add some noise
